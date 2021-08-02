083590d7191cd7131d06652eaf0d705994cf506e
2047241149 <2047241149@qq.com>
2021-08-03 00:15:55 +0800

    readd yaml, but not as submodule

---
+++ SDEngine/ThirdParty/yaml-cpp/premake5.lua
@@ -0,0 +1,29 @@
+﻿project "yaml-cpp"
+    kind "StaticLib"
+	language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+		"src/**.h",
+		"src/**.cpp",
+		"include/**.h",
+    }
+
+	includedirs
+	{
+		"include",
+	}
+
+    filter "system:windows"
+        systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
